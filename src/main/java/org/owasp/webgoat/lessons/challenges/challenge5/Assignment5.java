70990d33cb661e602736113f3c94f8ac45e88785
Katie Horne <katie.horne@semgrep.com>
2025-07-09 11:01:44 -0500

    Update Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -57,3 +57,77 @@ public class Assignment5 implements AssignmentEndpoint {
     }
   }
 }
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class Assignment5 implements AssignmentEndpoint {
+
+  private final LessonDataSource dataSource;
+  private final Flags flags;
+
+  @PostMapping("/challenge/5")
+  @ResponseBody
+  public AttackResult login(
+      @RequestParam String username_login, @RequestParam String password_login) throws Exception {
+    if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
+      return failed(this).feedback("required4").build();
+    }
+    if (!"Larry".equals(username_login)) {
+      return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
+    }
+    try (var connection = dataSource.getConnection()) {
+      PreparedStatement statement =
+          connection.prepareStatement(
+              "select password from challenge_users where userid = '"
+                  + username_login
+                  + "' and password = '"
+                  + password_login
+                  + "'");
+      ResultSet resultSet = statement.executeQuery();
+
+      if (resultSet.next()) {
+        return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+      } else {
+        return failed(this).feedback("challenge.close").build();
+      }
+    }
+  }
+}
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class Assignment5 implements AssignmentEndpoint {
+
+  private final LessonDataSource dataSource;
+  private final Flags flags;
+
+  @PostMapping("/challenge/5")
+  @ResponseBody
+  public AttackResult login(
+      @RequestParam String username_login, @RequestParam String password_login) throws Exception {
+    if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
+      return failed(this).feedback("required4").build();
+    }
+    if (!"Larry".equals(username_login)) {
+      return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
+    }
+    try (var connection = dataSource.getConnection()) {
+      PreparedStatement statement =
+          connection.prepareStatement(
+              "select password from challenge_users where userid = '"
+                  + username_login
+                  + "' and password = '"
+                  + password_login
+                  + "'");
+      ResultSet resultSet = statement.executeQuery();
+
+      if (resultSet.next()) {
+        return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+      } else {
+        return failed(this).feedback("challenge.close").build();
+      }
+    }
+  }
+}
+
